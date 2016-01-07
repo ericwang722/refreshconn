7e974f4bdb91cf942f4adddab9ce789b762e97f5
ericwang722 <ericwang722@users.noreply.github.com>
2016-01-07 15:32:53 +0800

    Update refreshconn.sh
    update with the new S3 naming convension

--- scripts/refreshconn.sh
+++ scripts/refreshconn.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #download the key from S3
-aws s3 cp s3://$S3_BUNDLE_NAME/admin-key-pairs/ghe-admin-non-production-private-key ~/.ssh/ghe-admin-non-production-private-key.pem
-chmod 400 ~/.ssh/ghe-admin-non-production-private-key.pem
+aws s3 cp s3://$S3_BUNDLE_NAME/ghe-admin-private-key ~/.ssh/ghe-admin-private-key.pem
+chmod 400 ~/.ssh/ghe-admin-private-key.pem
 
 #get the current host ip using http://instance-data
 HOST_IP=$(curl http://instance-data/latest/meta-data/public-ipv4)
 
 #ssh into host and run contrack
-ssh -tt -i ~/.ssh/ghe-admin-non-production-private-key.pem ec2-user@$HOST_IP   \
+ssh -tt -i ~/.ssh/ghe-admin-private-key.pem ec2-user@$HOST_IP   \
 "sudo yum install conntrack-tools.x86_64 -y;sudo conntrack -D -p udp;sudo conntrack -F"
